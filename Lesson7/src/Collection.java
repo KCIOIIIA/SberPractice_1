2194385a58c0ca6eed11be182d47876fe9bb887a
Ксюша <xysha17@gmail.com>
2022-04-10 18:00:11 +0300

    Выполнено седьмое домашнее задание

--- Lesson7/src/Collection.java
+++ Lesson7/src/Collection.java
@@ -1,54 +1,14 @@
 public interface Collection{
 
-    default int size(){
-        int count = 0;
-        for (int i = 0; i <= count; i++) {
-            if (a[i] != null) {
-                count = count + 1;
-            }
-        }
-        return count;
-    }
+    int size();
 
-    default boolean isEmpty(){
-        boolean isNotNull = false;
-        for (int i = 0; i <= size(); i++) {
-            if (a[i] != null) {
-                isNotNull = true;
-            }
-        }
-        return isNotNull;
-    }
+    boolean isEmpty();
 
-    default boolean constains(Object item){
-        boolean isMatch = false;
-        for (int i = 0; i <= size(); i++) {
-            if (a[i] == item) {
-                isMatch = true;
-            }
-        }
-        return isMatch;
-    }
+    boolean contains(Object item);
 
-    default boolean add(Object item){
-        a[size() + 1] = item;
-        return true;
-    }
+    boolean add(Object item);
 
-    default boolean remove(Object item) {
-        boolean isDelete = false;
-        for (int i = 0; i <= size(); i++) {
-            if (a[i] == item) {
-                a[i] = null;
-                isDelete = true;
-            }
-        }
-        return isDelete;
-    }
+    boolean remove(Object item);
 
-    default void clear(){
-        for (int i = 0; i <= size(); i++) {
-            a[i] = null;
-        }
-    }
+    void clear();
 }
--- Lesson7/src/List.java
+++ Lesson7/src/List.java
@@ -1,56 +1,16 @@
 public interface List extends Collection {
 
-    default void add(int index, Object item) {
-        a[index] = item;
-        for(int i = index + 1; i <= size() + 1; i ++){
-            a[i] = a[i+1];
-        }
-    }
+    void add(int index, Object item);
 
-    default void set(int index, Object item){
-        a[index] = item;
-    }
+    void set(int index, Object item);
 
-    default Object get(int index) throws IndexOutOfBoundsException{
-        if ((isEmpty() == false)&(a[index] != null)){
-            return a[index];
-        }
-        return null;
-    }
+    Object get(int index);
 
-    default int indexOf(Object item){
-        int j = -1;
-        boolean isFirst = false;
-        for (int i = 0; i <= size(); i++){
-            if ((a[i] == item)&(isFirst == false)){
-                j = i;
-                isFirst = true;
-            }
-        }
-        return j;
-    }
+    int indexOf(Object item);
 
-    default int lastIndexOf(Object item){
-        int j = -1;
-        for (int i = 0; i <= size(); i++){
-            if (a[i] == item){
-                j = i;
-            }
-        }
-        return j;
-    }
+    int lastIndexOf(Object item);
 
-    default Object remove(int index) throws IndexOutOfBoundsException{
-        Object copyA;
-        if (a[index] != null){
-            copyA = a[index];
-            a[index] = null
-            return copyA;
-        }
-        return null;
-    }
+    Object remove(int index);
 
-    default List subList(int from, int to) throws IndexOutOfBoundsException{
-        return null;
-    };
+    Object[] subList(int from, int to);
 }
